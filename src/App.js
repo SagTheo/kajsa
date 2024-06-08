591fd70727f2c96da367522128c67bf21a50aad1
Theo <sagniez.theo@laposte.net>
2024-06-08 13:55:06 +0200

    added more controls on dates

--- src/App.js
+++ src/App.js
@@ -27,24 +27,50 @@ function App() {
 
       if (Number(dateSplit[0]) < 1 || Number(dateSplit[0]) > 12) {
         alert(`The month for ${nameDate} must be between 01 and 12 included`)
+
+        return 
       }
 
       if (Number(dateSplit[1]) < 1 || Number(dateSplit[1]) > 31) {
         alert(`The day for ${nameDate} must be between 01 and 31 included`)
+
+        return 
       }
     } else {
       alert(nameDate + ' must have the mm/dd/yyyy format')
+
+      return
     }
+
+    return true
   }
 
   const chronology = (arrivalDate, endDate) => {
+    const arrivalDateSplit = arrivalDate.split('/')
+    const endDateSplit = endDate.split('/')
+
+    if (Number(endDateSplit[2]) < Number(arrivalDateSplit[2])) {
+      alert('The year for the end date can not be anterior to the year for the arrival date')
+    }
+
+    if (Number(endDateSplit[2]) === Number(arrivalDateSplit[2])) {
+      if (Number(endDateSplit[0]) < Number(arrivalDateSplit[0])) {
+        alert('The month for the end date can not be anterior to the month for the arrival date')
+      }  
+    }
     
+
+    if (Number(endDateSplit[0]) === Number(arrivalDateSplit[0]) && Number(endDateSplit[2]) === Number(arrivalDateSplit[2])) {
+      if (Number(endDateSplit[1]) < Number(arrivalDateSplit[1])) {
+        alert('The day for the end date can not be anterior to the day for the arrival date')
+      }
+    }
   }
 
   const checkForEvents = () => {
-    controlDate(arrivalDate, 'Arrival date')
-    controlDate(endDate, 'End date')
-    // console.log(`You are going to ${place} on ${arrivalDate} and you will leave on ${endDate}`)
+    if (controlDate(arrivalDate, 'Arrival date') && controlDate(endDate, 'End date')) {
+      chronology(arrivalDate, endDate)
+    }
   }
 
   return (
