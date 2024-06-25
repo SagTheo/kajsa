f5d8e24e90bfdfd481e97e9a18564d99315e579f
Theo <sagniez.theo@laposte.net>
2024-06-25 11:07:26 +0200

    added styles to calendar icon + started working on getting date value from calendar

--- src/components/UserInputDate.js
+++ src/components/UserInputDate.js
@@ -6,29 +6,34 @@ import { useState } from 'react';
 
 const UserInputDate = ({name, placeholder, handleChange, errorMessage, red}) => {
   const [displayCalendar, setDisplayCalendar] = useState(false)
+  const [date, setDate] = useState()
 
   return (
     <div className={styles.container}>
-      <input type='text' 
-              name={name} 
-              id={name} 
-              placeholder={placeholder}
-              onChange={handleChange}
-              className={`${red ? styles.outlineRed : styles.noOutline} ${styles.userInput}`}
-      />
-      <svg xmlns="http://www.w3.org/2000/svg" 
-           width="16" 
-           height="16" 
-           fill="currentColor" 
-           className="bi bi-calendar" 
-           viewBox="0 0 16 16"
-           onClick={() => setDisplayCalendar(!displayCalendar)}
-      >
-        <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
-      </svg>
+      <div className={styles.inputAndIcon}>
+        <input type='text' 
+                name={name} 
+                id={name} 
+                placeholder={placeholder}
+                onChange={handleChange}
+                value={date}
+                className={`${red ? styles.outlineRed : styles.noOutline} ${styles.userInput}`}
+        />
+        <svg xmlns="http://www.w3.org/2000/svg" 
+            width="20" 
+            height="20" 
+            fill="black" 
+            className={styles.calendar} 
+            viewBox="0 0 16 16"
+            onClick={() => setDisplayCalendar(!displayCalendar)}
+        >
+          <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
+        </svg>
+      </div>
+      {/* https://www.npmjs.com/package/react-calendar */}
       {
         displayCalendar ?
-        <Calendar />
+        <Calendar onChange={(value) => setDate(value)} />
         :
         null
       }
